f8e7871214fb3936c5276f85ae74489672d84036
Lexaxella <aleksisem00@gmail.com>
2021-10-18 18:41:41 +0700

    Добавлены комментарии

--- client.py
+++ client.py
@@ -1,19 +1,24 @@
 import socket
 import threading
 
+# Прослушивает сервер и отправляет никнейм
 def receive():
   while True:
     try:
+      # Принимает сообщения с сервера
+      # Если сообщение 'NICKNAME', отправляет никнейм
       message = client.recv(1024).decode('utf-8')
       if message == 'NICKNAME':
         client.send(nickname.encode('utf-8'))
       else:
         print(message)
     except:
+      # Закрывает соединение
       print('Error!')
       client.close()
       break
 
+# Отправляет сообщение на сервер
 def write():
   while True:
     message = '{}: {}'.format(nickname, input(''))
@@ -23,13 +28,17 @@ SERVER_HOST = '127.0.0.1'
 SERVER_PORT = 8080
 server_address = (SERVER_HOST, SERVER_PORT)
 
-nickname = input('Choose your nickname: ')
+# Выбор никнейма
+nickname = input('Выберите никнейм: ')
 
+# Подключение к серверу
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(server_address)
 
+# Запускает поток для прослушивания сервера
 receive_thread = threading.Thread(target = receive)
 receive_thread.start()
 
+# Запускает поток для отправления сообщений
 write_thread = threading.Thread(target = write)
 write_thread.start()
--- server.py
+++ server.py
@@ -1,28 +1,35 @@
 import socket
 import threading
 
+# Параметры соединения
 HOST = '127.0.0.1'
 PORT = 8080
 
+# Запуск сервера
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((HOST, PORT))
 server.listen()
 
+# Список клиентов и никнеймов
 clients = []
 nicknames = []
 
+# Отправляет сообщения всем подключенным клиентам
 def broadcast(sender, message):
   for client in clients:
     if (client == sender):
       continue
     client.send(message)
 
+# Отлавливает сообщения, отправляемые клиентами
 def handle(client):
   while True:
     try:
+      # Рассылает сообщение всем клиентам
       message = client.recv(1024)
       broadcast(client, message)
     except:
+      # Удаляет и закрывает соединение с клиентом
       index = clients.index(client)
       clients.remove(client)
       client.close()
@@ -31,20 +38,25 @@ def handle(client):
       nicknames.remove(nickname)
       break
 
+# Прослушивает соединения
 def receive():
   while True:
+    # Принимает соединение
     client, address = server.accept()
     print('Подключение {}'.format(str(address)))
 
+    # Запрашивает и сохраняет никнейм клиента
     client.send('NICKNAME'.encode('utf-8'))
     nickname = client.recv(1024).decode('utf-8')
     nicknames.append(nickname)
     clients.append(client)
 
+    # Уведломляет всех клиентов о подключении нового клиента
     print('Никнейм: {}'.format(nickname))
     broadcast('{} подключен'.format(nickname).encode('utf-8'))
     client.send('Подключен к серверу'.encode('utf-8'))
 
+    # Запуск потока по захвату сообщений
     thread = threading.Thread(target = handle, args = (client, ))
     thread.start()
 
